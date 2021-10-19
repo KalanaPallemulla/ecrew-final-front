b4c6a6175888d6eb0b985a45079374fcbbe3d856
Chanaka97n <chanaka97n@gmail.com>
2021-10-19 22:39:16 +0530

    added

--- src/Admin/AddingForm.js
+++ src/Admin/AddingForm.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { TimePicker } from "antd";
+import "antd/dist/antd.css";
+import moment from "moment";
 
 export default function AddingForm() {
+  const format = "HH:mm";
+
   return (
     <div className="md:grid md:grid-cols-2 gap-4 md:py-12 py-8">
       <div className="md:px-12">
@@ -38,43 +43,16 @@ export default function AddingForm() {
 
             <div className="mb-4 flex flex-row">
               <div>
-                {" "}
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
                   for="username"
                 >
-                  open Time
+                  Closing Time
                 </label>
-                <div className="container mx-auto">
-                  <div className="inline-flex text-lg border rounded-md shadow-lg p-2">
-                    <select
-                      name=""
-                      id=""
-                      className="px-2 outline-none appearance-none bg-transparent"
-                    >
-                      <option value="01">01</option>
-                      <option value="02">02</option>
-                      <option value="02">03</option>
-                    </select>
-                    <span className="px-2">:</span>
-                    <select
-                      name=""
-                      id=""
-                      className="px-2 outline-none appearance-none bg-transparent"
-                    >
-                      <option value="00">00</option>
-                      <option value="01">01</option>
-                    </select>
-                    <select
-                      name=""
-                      id=""
-                      className="px-2 outline-none appearance-none bg-transparent"
-                    >
-                      <option value="AM">AM</option>
-                      <option value="PM">PM</option>
-                    </select>
-                  </div>
-                </div>
+                <TimePicker
+                  defaultValue={moment("00:00", format)}
+                  format={format}
+                />{" "}
               </div>
 
               <div className="md:pl-12">
@@ -87,39 +65,13 @@ export default function AddingForm() {
                   >
                     Closing Time
                   </label>
-                  <div className="container mx-auto">
-                    <div className="inline-flex text-lg border rounded-md shadow-lg p-2">
-                      <select
-                        name=""
-                        id=""
-                        className="px-2 outline-none appearance-none bg-transparent"
-                      >
-                        <option value="01">01</option>
-                        <option value="02">02</option>
-                        <option value="02">03</option>
-                      </select>
-                      <span className="px-2">:</span>
-                      <select
-                        name=""
-                        id=""
-                        className="px-2 outline-none appearance-none bg-transparent"
-                      >
-                        <option value="00">00</option>
-                        <option value="01">01</option>
-                      </select>
-                      <select
-                        name=""
-                        id=""
-                        className="px-2 outline-none appearance-none bg-transparent"
-                      >
-                        <option value="AM">AM</option>
-                        <option value="PM">PM</option>
-                      </select>
-                    </div>
-                  </div>
+                  <TimePicker
+                    defaultValue={moment("00:00", format)}
+                    format={format}
+                  />{" "}
                 </div>
               </div>
-              <div className="">
+              <div className="ml-36">
                 <label className="flex flex-col items-center px-2 py-3 bg-white rounded-md shadow-md tracking-wide uppercase border border-blue cursor-pointer hover:bg-purple-600 hover:text-white text-purple-600 ease-linear transition-all duration-150">
                   <i className="fas fa-cloud-upload-alt "></i>
                   <span className="mt-2 text-xs leading-normal">
@@ -138,7 +90,6 @@ export default function AddingForm() {
             <tr>
               <th className="w-1/4 px-2">Name</th>
               <th className="w-1/4 px-2">Image</th>
-              <th className="w-1/4 px-2">Add</th>
               <th className="w-1/4 px-2">Delete</th>
             </tr>
           </thead>
@@ -146,14 +97,11 @@ export default function AddingForm() {
             <tr className="border-t ">
               <td className="p-2">Intro to CSS</td>
               <td className="p-2">Adam</td>
-              <td className="p-2">858</td>
-              <td className="p-2">858</td>
-            </tr>
-            <tr className="border-t ">
-              <td className="p-2">Intro to CSS</td>
-              <td className="p-2">Adam</td>
-              <td className="p-2">858</td>
-              <td className="p-2">858</td>
+              <td className="p-2">
+                <button class="bg-transparent hover:bg-red-500 text-red-700  hover:text-white py-1 px-2 border border-red-500 hover:border-transparent rounded">
+                  Button
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
